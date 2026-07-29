7ae8b328e163f417561cecb9ae503bf5abd20cf1
marccasals98 <marc.casals8@gmail.com>
2026-07-29 15:57:16 +0200

    sox was not supported anymore

--- scripts/augmentation.py
+++ scripts/augmentation.py
@@ -38,55 +38,63 @@ class DataAugmentator:
         augmentation_window_size_secs,
         augmentation_effects,
     ):
-        
+
         self.augmentation_directory = augmentation_noises_directory # Background noises directory
         self.rirs_directory = augmentation_rirs_directory # RIRs directory
         self.window_size_secs = augmentation_window_size_secs
         self.augmentation_effects = augmentation_effects
 
         self.create_augmentation_list(augmentation_noises_labels_path)
-        self.create_rir_list(augmentation_rirs_labels_path) 
+        self.create_rir_list(augmentation_rirs_labels_path)
 
-        # TODO move to settings         
+        # TODO move to settings
         self.SPEEDS = ["0.9", "1.1"]
         self.SNR_NOISE_RANGE = [15, 25]
         self.SNR_SPEECH_RANGE = [15, 25]
         self.SNR_MUSIC_RANGE = [15, 20]
-        
-    
+
+
     def create_augmentation_list(self, augmentation_labels_path):
-        
+
         with open(augmentation_labels_path) as handle:
             self.augmentation_list = handle.readlines()
-    
+
 
     def create_rir_list(self, rirs_labels_path):
-        
+
         with open(rirs_labels_path) as handle:
             self.rirs_list = handle.readlines()
-    
+
 
     def apply_speed_perturbation(self, audio, sample_rate):
-            
+
         speed = random.choice(self.SPEEDS)
 
-        augmented_audio_waveform, augmented_sample_rate = torchaudio.sox_effects.apply_effects_tensor(
-            audio, sample_rate, [["speed", speed]]
+        # not available anymore:
+        # augmented_audio_waveform, augmented_sample_rate = torchaudio.sox_effects.apply_effects_tensor(
+        #     audio, sample_rate, [["speed", speed]]
+        # )
+        intermediate_rate = int(sample_rate * float(speed))
+
+        augmented_audio_waveform = torchaudio.functional.resample(
+            waveform = audio,
+            orig_freq = sample_rate,
+            new_freq = intermediate_rate
         )
 
         # Speed perturbation changes only the sampling rate, so we need to resample to the original sample rate
         resampled_waveform = torchaudio.functional.resample(
             waveform = augmented_audio_waveform,
-            orig_freq = augmented_sample_rate, 
-            new_freq = sample_rate, 
+            orig_freq = intermediate_rate,
+            new_freq = sample_rate,
         )
-        
+
         # We return the resampled waveform, the sample rate remains the original
         return resampled_waveform
-    
-    
+
+
     def apply_reverb(self, audio, sample_rate):
-            
+
         if self.rirs_directory is not None:
             path = os.path.join(self.rirs_directory, random.choice(self.rirs_list).strip())
         else:
@@ -98,8 +106,8 @@ class DataAugmentator:
             logger.warning(f"resampling from {rir_sample_rate} to {sample_rate}")
             rir_wav = torchaudio.functional.resample(
                 waveform = rir_wav,
-                orig_freq = rir_sample_rate, 
-                new_freq = sample_rate, 
+                orig_freq = rir_sample_rate,
+                new_freq = sample_rate,
             )
             rir_sample_rate = sample_rate
 
@@ -107,12 +115,12 @@ class DataAugmentator:
         # Clean up the RIR,  extract the main impulse, normalize the signal power
         normalized_rir = rir_wav[:, int(rir_sample_rate * 0.01) : int(rir_sample_rate * 1.3)]
         normalized_rir = normalized_rir / torch.norm(normalized_rir, p=2)
-        
+
         augmented_waveform = torch.mean(torchaudio.functional.fftconvolve(audio, normalized_rir), dim=0).unsqueeze(0)
 
         return augmented_waveform
-            
-    
+
+
     def get_SNR_bounds(self, background_audio_type):
 
         if background_audio_type == "noise":
@@ -123,30 +131,30 @@ class DataAugmentator:
             return self.SNR_MUSIC_RANGE
         else:
             return self.SNR_NOISE_RANGE
-            
-    
+
+
     def sample_random_SNR(self, background_audio_type):
 
         snr_bounds = self.get_SNR_bounds(background_audio_type)
-        
+
         return random.uniform(snr_bounds[0], snr_bounds[1])
-    
-    
+
+
     def crop_noise(self, noise, noise_sample_rate, window_size_secs):
 
         noise_duration_samples = noise.size()[1]
         noise_duration_secs = noise_duration_samples / noise_sample_rate
         window_size_samples = int(window_size_secs * noise_sample_rate)
-        
+
         if noise_duration_secs <=  window_size_secs:
             cropped_noise = noise[:, :]
         else:
             start = random.randint(0, noise_duration_samples - window_size_samples)
             end = start + window_size_samples
             cropped_noise = noise[:, start : end]
-        
+
         return cropped_noise
-    
+
 
     def pad_noise(self, noise, audio):
 
@@ -155,10 +163,10 @@ class DataAugmentator:
         cropped_noise_padded = torch.nn.functional.pad(noise, (pad_left, 0), mode = "constant")
 
         return cropped_noise_padded
-    
-    
+
+
     def add_background_noise(self, audio, sample_rate):
-            
+
         background_audio_line = random.choice(self.augmentation_list).strip()
 
         background_audio_name = background_audio_line.split("\t")[0].strip()
@@ -175,15 +183,15 @@ class DataAugmentator:
             logger.warning(f"resampling from {noise_sample_rate} to {sample_rate}")
             noise = torchaudio.functional.resample(
                 waveform = noise,
-                orig_freq = noise_sample_rate, 
-                new_freq = sample_rate, 
+                orig_freq = noise_sample_rate,
+                new_freq = sample_rate,
             )
             noise_sample_rate = sample_rate
 
         # TODO first loading the audio and then cropping is unefficient
         cropped_noise = self.crop_noise(
-            noise, 
-            noise_sample_rate, 
+            noise,
+            noise_sample_rate,
             min(self.window_size_secs, int(audio.size()[1] / sample_rate)),
         )
 
@@ -196,46 +204,46 @@ class DataAugmentator:
         noisy_audio = torchaudio.functional.add_noise(audio, padded_cropped_noise, audio_SNR)
 
         return noisy_audio
-        
-    
+
+
     def augment(self, audio, sample_rate):
-        
+
         effect = random.choice(self.augmentation_effects)
 
         logger.debug(f"Data augmentation {effect} is going to be applied...")
-        
+
         # getattr(self, effect) is equivalent to apply self.effect(audio, sample_rate)
 
         augmented_waveform = getattr(self, effect)(audio, sample_rate)
 
         return augmented_waveform
 
-    
+
     def __call__(self, audio, sample_rate):
-        
+
         return self.augment(audio, sample_rate)
 
-        
+
     def __len__(self):
 
         return len(self.augmentation_list)
 
-        
 
-        
 
-        
 
-        
 
-        
 
-        
 
-        
 
-        
 
-        
 
-        
+
+
+
+
+
+
+
+
+
+
