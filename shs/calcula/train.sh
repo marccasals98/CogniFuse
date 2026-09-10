1462eb2117722ad4df7de767d45c3652a95ee8bf
marccasals98 <marc.casals8@gmail.com>
2026-09-10 17:19:08 +0200

    Sorry my last commit is an error, I thought I was in another branch

--- shs/calcula/train.sh
+++ shs/calcula/train.sh
@@ -80,7 +80,6 @@ uv run torchrun --nproc_per_node=1 --standalone scripts/train.py \
 	--transcription_cache_dir '/home/usuaris/veussd/marc.casals/datasets/WAB_samples/trans_w40_s7.00_medium_metadata' \
 	--whisper_model_name medium\
 	--whisper_language es\
-	--use_weights_and_biases \
-	--simple_dataset
+	--use_weights_and_biases
 
 date
